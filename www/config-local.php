d24f952d7efbc69efa002a9cbcc5917c98b61597
Eric Miller <github@patternleaf.com>
2014-09-09 15:59:29 -0600

    Continued setup.

---
+++ www/config-local.php
@@ -0,0 +1,20 @@
+<?php
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'andy_stone_dev');
+
+/** MySQL database username */
+define('DB_USER', 'username');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
