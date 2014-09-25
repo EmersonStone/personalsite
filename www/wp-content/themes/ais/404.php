8d5fa226355e1d970fe034bf447ca0db02f319d0
Andy Stone <because3@host131.hostmonster.com>
2014-09-24 22:41:03 -0600

    404-ing

--- www/wp-content/themes/ais/404.php
+++ www/wp-content/themes/ais/404.php
@@ -10,9 +10,14 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<section class="error-404 not-found">
+			<section class="error-404 not-found container">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Sorry. You definitely found the wrong page.', 'ais' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'You definitely found the wrong page', 'ais' ); ?></h1>
+					<div class="writing">
+						<p>Sorry, you seem to have stumbled upon the 404 page. If you clicked on a link that brought you here, please <a href="/keeping-in-touch">contact me</a> and I'll look in to it right away. Thanks and enjoy the rest of your day.</p>
+					</div>
+
+
 				</header><!-- .page-header -->
 
 			</section><!-- .error-404 -->
