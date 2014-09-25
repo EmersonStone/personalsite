e36f25b832af00257982d04199b8a4eba6c564ca
Andy Stone <because3@host131.hostmonster.com>
2014-09-24 22:30:17 -0600

    Changed a few things in the header.php to lessen the load

--- www/wp-content/themes/ais/header.php
+++ www/wp-content/themes/ais/header.php
@@ -10,16 +10,17 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
+<?php /*
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+*/ ?>
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0 user-scalable=no">
 <meta name="description" content="My name is Andy Stone, and I am a freelance digital designer living and designing in Boulder, CO. I work on websites, branding projects, and iPhone apps. I have experience in UI, UX, IA and every other acronym that you can think of.">
 <meta name="keywords" content="design, freelance, contract, designer, boulder, colorado, denver, iphone, android, ios, app, responsive web design, branding, logo, logo designer, branding, user interfaces, user experience design">
 <meta name="author" content="Andy Stone">
-<link rel="shortcut icon" href="ico/favicon.png">
+<link rel="shortcut icon" href="favicon.ico">
 
 <?php wp_head(); ?>
 </head>
@@ -39,5 +40,5 @@
 					</a>
 				</div>
 			</div>
-			<div class="bottomborder noimage"></div>
+			<div class="bottomborder <?php echo is_front_page() ? ' noimage' : '';?>"></div>
 
