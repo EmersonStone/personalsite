7a8245ce8af791a934740342dd07ee09f6194a74
Andy Stone <because3@host131.hostmonster.com>
2014-09-24 22:45:31 -0600

    Styled simple 404 page

--- www/wp-content/themes/ais/404.php
+++ www/wp-content/themes/ais/404.php
@@ -12,9 +12,9 @@ get_header(); ?>
 
 			<section class="error-404 not-found container">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'You definitely found the wrong page', 'ais' ); ?></h1>
 					<div class="writing">
-						<p>Sorry, you seem to have stumbled upon the 404 page. If you clicked on a link that brought you here, please <a href="/keeping-in-touch">contact me</a> and I'll look in to it right away. Thanks and enjoy the rest of your day.</p>
+						<h2 class="page-title"><?php _e( 'Sorry! You probably do not mean to come here.', 'ais' ); ?></h2>
+						<p>You seem to have stumbled upon the 404 page. If you clicked on a link that brought you here, please <a href="/keeping-in-touch">contact me</a> and I'll look in to it right away. Thanks and enjoy the rest of your day.</p>
 					</div>
 
 
